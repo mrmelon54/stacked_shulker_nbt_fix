48b70d8e09b3f52ef406a8bbbb15a0b1d98fb626
MrMelon54 <github@mrmelon54.com>
2023-06-13 12:07:21 +0100

    Remove extra block entity tag from shulker box when there are no items

--- common/src/main/java/com/mrmelon54/StackedShulkerNbtFix/StackedShulkerNbtFix.java
+++ common/src/main/java/com/mrmelon54/StackedShulkerNbtFix/StackedShulkerNbtFix.java
@@ -1,9 +1,59 @@
 package com.mrmelon54.StackedShulkerNbtFix;
 
+import dev.architectury.event.EventResult;
+import dev.architectury.event.events.common.PlayerEvent;
+import net.minecraft.nbt.CompoundTag;
+import net.minecraft.nbt.ListTag;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.Items;
+
 public class StackedShulkerNbtFix {
     public static final String MOD_ID = "stacked_shulker_nbt_fix";
 
+    public static Item[] SHULKERS = new Item[]{
+            Items.SHULKER_BOX,
+            Items.WHITE_SHULKER_BOX,
+            Items.ORANGE_SHULKER_BOX,
+            Items.MAGENTA_SHULKER_BOX,
+            Items.LIGHT_BLUE_SHULKER_BOX,
+            Items.YELLOW_SHULKER_BOX,
+            Items.LIME_SHULKER_BOX,
+            Items.PINK_SHULKER_BOX,
+            Items.GRAY_SHULKER_BOX,
+            Items.LIGHT_GRAY_SHULKER_BOX,
+            Items.CYAN_SHULKER_BOX,
+            Items.PURPLE_SHULKER_BOX,
+            Items.BLUE_SHULKER_BOX,
+            Items.BROWN_SHULKER_BOX,
+            Items.GREEN_SHULKER_BOX,
+            Items.RED_SHULKER_BOX,
+            Items.BLACK_SHULKER_BOX,
+    };
+
     public static void init() {
-        System.out.println(ExampleExpectPlatform.getConfigDirectory().toAbsolutePath().normalize().toString());
+        PlayerEvent.PICKUP_ITEM_PRE.register((player, entity, stack) -> {
+            for (Item shulker : SHULKERS) {
+                if (!stack.is(shulker)) continue;
+
+                // get nbt tag
+                CompoundTag orCreateTag = stack.getOrCreateTag();
+
+                // get block entity tag
+                if (!orCreateTag.contains("BlockEntityTag", 10)) continue;
+                CompoundTag blockEntityTag = orCreateTag.getCompound("BlockEntityTag");
+
+                // get items list
+                if (!blockEntityTag.contains("Items", 9)) continue;
+                ListTag items = blockEntityTag.getList("Items", 10);
+
+                // if there are no items then remove it
+                if (items.size() != 0) continue;
+
+                orCreateTag.remove("BlockEntityTag");
+                stack.setTag(orCreateTag);
+                entity.setItem(stack);
+            }
+            return EventResult.pass();
+        });
     }
 }
